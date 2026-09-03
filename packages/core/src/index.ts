05f6a4b081a365ae61f2f0591d5a5ef643af7239
yishu-ziyu <yishuziyu@gmail.com>
2026-09-03 15:45:09 +0800

    feat(core): 搬入抓取与来源层级，新增 webFetch、pivot 抽取与出处簇

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,3 @@
 export const PACKAGE = "@rhg/core" as const;
+export * from "./fetch/index.js";
+export * from "./rules/index.js";
---
+++ packages/core/src/rules/index.ts
@@ -0,0 +1,2 @@
+export { tierOf } from "./sourceTiers.js";
+export { TIER_A_HOSTS, TIER_A_SUFFIXES, TIER_B_HOSTS } from "./sourceTiers.data.js";
